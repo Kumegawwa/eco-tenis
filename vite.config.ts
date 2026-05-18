fa22d32830ef03d6ed472102b6d71f9f777193b2
Lucas Kumegawa <lucaskumegawa222@gmail.com>
2026-05-17 22:04:51 -0300

    fix(deploy): adicionar adaptador nitro com preset vercel para correcao do erro 404 no ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import tsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import tailwindcss from "@tailwindcss/vite";
-
-// Forçamos o motor a construir para a Vercel, ignorando a Cloudflare
-process.env.NITRO_PRESET = "vercel";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [
-    // Ao deixarmos vazio, ignoramos o server.ts antigo da Lovable
-    tanstackStart(), 
-    viteReact(),
-    tailwindcss(),
-    tsConfigPaths(),
-  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+      tsConfigPaths(),
+      nitro({
+        preset: "vercel",
+      }),
+    ],
+  },
 });
